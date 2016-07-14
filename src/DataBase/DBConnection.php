454592d33fc0e61cfb76caf260c3d9c0e770f40d
Tika Datta Pahadi <tika.pahadi@fusionstak.com>
2016-07-13 22:25:21 -0500

    Changes

---
+++ src/DataBase/DBConnection.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace hooks\DataBase;
+
+
+class DBConnection extends \PDO
+{
+
+    public function __construct($host = DB_DSN, $db = DB_NAME, $user = DB_USER, $pass = DB_PASS)
+    {
+        try{
+            parent::__construct($host.';dbname='.$db, $user, $pass, [ \PDO::ATTR_PERSISTENT => true ]);
+            $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, true);
+            $this->setAttribute(\PDO::ATTR_PERSISTENT, true);
+        } catch (\Exception $e){
+            errorLog("Database Connection Failed");
+            die("Database Connection Failed");
+        }
+    }
+
+}
